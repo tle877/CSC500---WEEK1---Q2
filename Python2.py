6e72dbafc04b22b6f0346e40e96e7a1b17b2f5f2
tle877 <60452318+tle877@users.noreply.github.com>
2024-02-14 15:36:47 -0800

    Update Python2.py

--- Python2.py
+++ Python2.py
@@ -6,7 +6,7 @@ multiplication_result = num1 * num2
 if num2 != 0:
     division_result = num1 / num2
 else:
-    division_result = "Division by zero is undefined"
+    division_result = "Error !!! Division by zero is not allowed"
 
 print("The multiplication of", num1, "and", num2, "is:", multiplication_result)
 print("The division of", num1, "by", num2, "is:", division_result)
